3ac9cefb1f2196583f0f31d7bbc2ef1be73d47ce
Alpesh_Rajendra <alpeshrajendra@gmail.com>
2026-02-11 15:13:51 +0530

    Update S3 bucket name and region in backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "super-mario-tf-kubernetes-project" # Replace with your actual S3 bucket name
+    bucket = "super-mario-tf-project" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-southeast-1"
+    region = "eu-west-1"
     profile = "eks"
   }
 }
